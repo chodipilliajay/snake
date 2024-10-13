cf8ae91df48f5dfcced9818d8d69e43668d08ab9
Chodipilli Ajay <115925992+chodipilliajay@users.noreply.github.com>
2024-10-13 09:55:14 +0530

    Add files via upload

---
+++ day-20/pythonProject1/scoreboard.py
@@ -0,0 +1,39 @@
+from tkinter.constants import CENTER
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+ALIGN = "center"
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=CENTER, font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(file="data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=CENTER, font=FONT)
